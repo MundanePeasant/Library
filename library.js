33ebcbc09bc6886ddc5cb9f964aa743d6020077e
Austin <vandersteen.austin@gmail.com>
2023-03-29 20:23:10 -0500

    Instantiate all files needed

---
+++ library.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function  Book(title, author, pages, read) {
+    //Book constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(){
+    //do stuff here
+}
+
+function removeBookFromLibrary(){
+    //do stuff here
+}
