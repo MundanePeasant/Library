68d6722b4ea2e120ac1de2b559ea66796c026c25
Austin <vandersteen.austin@gmail.com>
2023-03-29 20:58:52 -0500

    Add functionality to show and hide the popup/form

--- library.js
+++ library.js
@@ -9,9 +9,37 @@ function  Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(){
-    //do stuff here
+    //construct new Book object and add it to the Library array
+    //Loop through library array and have all books displayed on page
 }
 
 function removeBookFromLibrary(){
-    //do stuff here
+    //retrieve the object reference (or something that references the book)
+    //remove that book from the library array
+    //reload the Library
 }
+
+function loadLibrary(myLibrary){
+    //loop through myLirbary and add all books within it to the page
+    //1. remove all existing books
+    //2. load books from myLibrary array and insert the associated html
+}
+
+
+//Event Listeners
+
+const addButton = document.getElementById("add-book");
+const popup = document.getElementsByClassName("popup")[0];
+
+addButton.addEventListener("click", () => {
+    popup.style.display = "block";
+});
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    popup.style.display="none";
+    //also need to put some validations on the form in html
+    //send form data to addBookFromLibrary
+});
